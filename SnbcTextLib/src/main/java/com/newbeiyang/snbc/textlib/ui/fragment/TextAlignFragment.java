7c58793de44e8e925275810339a1086338cd2ab1
zhangyuhui@newbeiyang.com <darlyyuhui@hotmail.com>
2018-12-13 10:02:30 +0800

    对齐缩进界面编辑
    {
    对齐缩进界面进行编写
    }
    
    Co-Authored-By: Darly.Fronch <darlyyuhui@users.noreply.github.com>

--- SnbcTextLib/src/main/java/com/newbeiyang/snbc/textlib/ui/fragment/TextAlignFragment.java
+++ SnbcTextLib/src/main/java/com/newbeiyang/snbc/textlib/ui/fragment/TextAlignFragment.java
@@ -1,6 +1,7 @@
 package com.newbeiyang.snbc.textlib.ui.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 
 import com.newbeiyang.snbc.textlib.R;
 import com.newbeiyang.snbc.textlib.common.listener.OnEditSupernatantListener;
@@ -13,10 +14,11 @@ import com.newbeiyang.snbc.textlib.common.listener.OnEditSupernatantListener;
  * 公司：山东新北洋信息技术股份有限公司西安分公司
  * 邮箱：zhangyuhui@newbeiyang.com
  */
-public class TextAlignFragment extends BaseTextFragment {
+public class TextAlignFragment extends BaseTextFragment implements TabLayout.OnTabSelectedListener {
 
     private OnEditSupernatantListener textAlignListener;
 
+    private TabLayout id_supernatant_align_tab;
 
     @Override
     protected int root() {
@@ -25,16 +27,30 @@ public class TextAlignFragment extends BaseTextFragment {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        id_supernatant_align_tab = rootView.findViewById(R.id.id_supernatant_align_tab);
     }
 
     @Override
     protected void loadData() {
+        TabLayout.Tab left = id_supernatant_align_tab.newTab();
+        left.setTag("LEFT");
+        left.setIcon(R.drawable.dr_natant_text_left_select);
+        id_supernatant_align_tab.addTab(left);
 
+        TabLayout.Tab middle = id_supernatant_align_tab.newTab();
+        middle.setTag("MIDDLE");
+        middle.setIcon(R.drawable.dr_natant_text_middle_select);
+        id_supernatant_align_tab.addTab(middle);
+
+        TabLayout.Tab right = id_supernatant_align_tab.newTab();
+        right.setTag("RIGHT");
+        right.setIcon(R.drawable.dr_natant_text_right_select);
+        id_supernatant_align_tab.addTab(right);
     }
 
     @Override
     protected void initListener() {
-
+        id_supernatant_align_tab.addOnTabSelectedListener(this);
     }
 
 
@@ -42,5 +58,26 @@ public class TextAlignFragment extends BaseTextFragment {
     public void setAlignListener(OnEditSupernatantListener listener) {
         super.setAlignListener(listener);
         textAlignListener = listener;
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        if ("LEFT".equals(tab.getTag())){
+            //点击左对齐
+        }else if ("MIDDLE".equals(tab.getTag())){
+            //点击居中
+        }else if ("RIGHT".equals(tab.getTag())){
+            //点击右对齐
+        }
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
